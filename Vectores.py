ff42b3f7b3757f8f786b9b1c23fe15f662dc6f25
GERO2007 <romellgerardomejiaramon@gmail.com>
2026-09-07 22:53:13 -0600

    Entrega de Vectores en clase.py

---
+++ Vectores.py
@@ -0,0 +1,20 @@
+def mostrar_vector(datos):
+    for elemento in datos:
+        print(elemento)
+
+def media(datos):
+    suma = sum(datos)
+    return suma / len(datos)
+
+def main():
+    pares = [2, 4, 6, 8, 10]
+    impares = [1, 3, 5, 7, 9]
+
+    mostrar_vector(pares)
+    print("Media = " + str(media(pares)))
+
+    mostrar_vector(impares)
+    print("Media = " + str(media(impares)))
+
+if __name__ == "__main__":
+    main()
