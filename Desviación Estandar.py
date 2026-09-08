c79b8b25b656a9380b47f21ed5e57a77a49ebe0d
GERO2007 <romellgerardomejiaramon@gmail.com>
2026-09-08 13:05:15 -0600

    Entrega de código moda, media, mediana, desviación estándar y varianza.py

--- Desviación Estandar.py
+++ Desviación Estandar.py
@@ -1,3 +1,4 @@
+import time 
 import random
 import statistics
 
@@ -13,6 +14,8 @@ def media(datos):
 
 
 def main():
+    inicio = time.time() # ← empieza a contar
+    
     datos = [random.randint(150, 250) for _ in range(50)]
 
     mostrar_vector(datos)
@@ -23,6 +26,8 @@ def main():
     print("Varianza = " + str(statistics.pvariance(datos)))
     print("Desviación estandar = " + str(statistics.pstdev(datos)))
 
-
+    fin = time.time() # ← termina de contar 
+    print("Tiempo de ejecución: " + str(fin - inicio) + " segundos")
+        
 if __name__ == "__main__":
     main()
